c71d7b91ed6ac1f7a911e5f1a487be76f22e73d1
Chocolatey <chocolatey@realdimensions.net>
2018-08-10 19:19:05 +0000

    AU: 1 updated - dacia-media-nav-toolbox [skip ci] https://gist.github.com/8fd85ef1acabc5dfd90afaf9db33920f/c0fd66aaca55983ffcd7622dcd30a579e44f3dfb

--- automatic/dacia-media-nav-toolbox/tools/chocolateyinstall.ps1
+++ automatic/dacia-media-nav-toolbox/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = 'http://azure.distrib.naviextras.com/content/!application/LG/ToolboxDacia/2016042601_3.18.5.647040_STRICT_dacia/Dacia_Media_Nav_Toolbox_Setup.exe'
-  checksum      = '7CFFBCDC04F0BC53D52521E7E75C85A17FE4D4023CE58A523D85CE5BCB9B1F32'
+  url           = 'http://azure.distrib.naviextras.com/content/!application/LG/ToolboxDacia/2018080701_3.18.5.740218_STRICT_dacia/Dacia_Media_Nav_Toolbox_Setup.exe'
+  checksum      = '496a42a56a164f549cb22489170c9f70e55066164aab9a6374d44537613f1764'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
