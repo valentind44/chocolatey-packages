7b6e2c3be016373f8a7f907f293e3bbf24c74509
Valentin Dumont <valentin.dumont@outlook.com>
2018-08-10 21:14:57 +0200

    All packages are now automatic !

---
+++ automatic/dacia-media-nav-toolbox/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  url           = 'http://azure.distrib.naviextras.com/content/!application/LG/ToolboxDacia/2016042601_3.18.5.647040_STRICT_dacia/Dacia_Media_Nav_Toolbox_Setup.exe'
+  checksum      = '7CFFBCDC04F0BC53D52521E7E75C85A17FE4D4023CE58A523D85CE5BCB9B1F32'
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
