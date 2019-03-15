272da5f7b094fe32142c05024d9f2eae2487ee13
Chocolatey <chocolatey@realdimensions.net>
2019-03-15 08:06:15 +0000

    AU: 1 updated - lgbridge [skip ci] https://gist.github.com/c1dd410e8e6ad71c91433b7910da167a/ff17407d2280f772f23c78d011fe17a0c055ee3c

--- automatic/lgbridge/tools/chocolateyinstall.ps1
+++ automatic/lgbridge/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   url           = 'http://tool.cdn.gdms.lge.com/dn/downloader.dev?fileKey=PWZO7A26B9UW'
-  checksum      = 'f3cccca6ba2ccafb626dc2b6131d30a594d53ad8469f14b91a69c341a988fa21'
+  checksum      = '57ce9bdfbecf5d3bd5a4f14ba86c2fa44bab944fa0459a52f2b4508ab6cecb46'
   checksumType  = 'sha256'
   silentArgs   = '/S'
 }
