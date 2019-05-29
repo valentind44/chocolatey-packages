044600e629dc3d60f3a722810da2ab4c0de8e34f
Valentin Dumont <valentin.dumont@etu.univ-nantes.fr>
2019-05-29 12:11:25 +0200

    Removed beforemodify script for lyricsfinder

--- automatic/lyricsfinder/tools/chocolateyBeforeModify.ps1
+++
@@ -1,4 +0,0 @@
-﻿$ErrorActionPreference = 'Stop';
-$toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-
-Remove-Item -path "$toolsDir\*" -exclude chocolateyBeforeModify.ps1,chocolateyInstall.ps1,chocolateyUninstall.ps1 -recurse
