48785780ef10c913737815735311f5a6cd03b93b
Chocolatey <chocolatey@realdimensions.net>
2019-11-26 00:07:05 +0000

    AU: 1 updated - molotov [skip ci] https://gist.github.com/0a26054704e5ac020ed1d5a5653cf02d/1edb34281189cd24e54951990431ff51461be22d

--- automatic/molotov/tools/chocolateyinstall.ps1
+++ automatic/molotov/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  url           = 'https://desktop-auto-upgrade.molotov.tv/win/ia32/MolotovSetup-4.2.0-ia32.exe'
-  url64bit      = 'https://desktop-auto-upgrade.molotov.tv/win/x64/MolotovSetup-4.2.0.exe'
-  checksum      = '3ba3b5a9f876bdf317f21dfaef504f50a16b1d60dd62ee04e8b0b5c1dea0fe35'
-  checksum64    = '04b3e27fd596a73e0b87d37281312dd8d426321f4911a8d6b94236799a4bf406'
+  url           = 'https://desktop-auto-upgrade.molotov.tv/win/ia32/MolotovSetup-4.2.1-ia32.exe'
+  url64bit      = 'https://desktop-auto-upgrade.molotov.tv/win/x64/MolotovSetup-4.2.1.exe'
+  checksum      = 'b8a4270277c26174dc95162f224502acc1a7bb980a76347c1424a8a467507ea3'
+  checksum64    = 'f08bb3cff076e310cedd4549e368e1b03ac38fc7976c2d194cd3d87d715f5851'
   checksumType  = 'sha256'
   silentArgs   = '/s'
 }
